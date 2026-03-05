846830e3575191cba90762a23fda4ed54062e83e
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-03-05 11:08:25 +0100

    plugin updated

---
+++ wp-content/themes/practicetech/page-contact.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Contact */ ?>
+
+<?php
+get_header();
+?>
+
+	<section class="hero-contact">
+		<div class="container">
+			<div class="row">
+				<h1>Contact</h1>
+			</div>
+		</div>
+	</section>
+
+<?php
+get_footer();
