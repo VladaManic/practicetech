bf4e1f4fac60a11a66055084f6dd68568e2186f0
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-03-09 10:10:03 +0100

    contact page

--- wp-content/themes/practicetech/page-contact.php
+++ wp-content/themes/practicetech/page-contact.php
@@ -12,5 +12,13 @@ get_header();
 		</div>
 	</section>
 
+	<div class="contact-form">
+		<div class="container">
+			<div class="row">
+				<?php echo do_shortcode('[contact-form-7 id="d216aa1" title="Contact form 1"]');; ?>
+			</div>
+		</div>
+	</div>
+
 <?php
 get_footer();
