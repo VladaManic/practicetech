c52815e582507eb39d72bf7a52f08b62e4113724
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-03-12 11:58:31 +0100

    more style

--- wp-content/themes/practicetech/template-parts/text-image.php
+++ wp-content/themes/practicetech/template-parts/text-image.php
@@ -1,22 +1,24 @@
-<section class="text-image">
+<?php
+	$order = get_sub_field('order');
+	$image = get_sub_field('image');
+	$title = get_sub_field('title');
+?>
+<section class="text-image <?php if($order == 'Right'){echo 'order1';} else {echo 'order2';} ?>">
 	<div class="container">
 		<div class="row">
-		<?php
-			$order = get_sub_field('order');
-			$image = get_sub_field('image');
-		?>
-			<div class="item-text <?php if($order == 'Right'){echo 'order1';} else {echo 'order2';} ?>">
+			<div class="item-text">
 				<div class="wrap">
 					<?php
-							$title = get_sub_field('title');
-							if ($title) {
-								?><h2><?php echo $title; ?></h2><?php
-							}
+						if ($title) {
+					?>
+							<h2><?php echo $title; ?></h2>
+					<?php
+						}
 					?>
 					<div class="content"><?php the_sub_field('text'); ?></div>
 				</div>
 			</div>
-			<div class="item-image <?php if($order == 'Right'){echo 'order2';} else {echo 'order1';} ?>" title="<?php echo $image['title']; ?>">
+			<div class="item-image" title="<?php echo $image['title']; ?>">
 				<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-inner" />
 			</div>
 		</div>
