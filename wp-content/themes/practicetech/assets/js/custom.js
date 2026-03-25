d05e844195c2df291a494b92363fdab0d2fb4795
VladaManic <65961459+VladaManic@users.noreply.github.com>
2026-03-25 11:39:11 +0100

    about hero

--- wp-content/themes/practicetech/assets/js/custom.js
+++ wp-content/themes/practicetech/assets/js/custom.js
@@ -36,6 +36,9 @@ jQuery(function ($) {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    autoplay: {
+      delay: 3000,
+    },
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
